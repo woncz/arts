4a52c797e268b726c8ef7bd368dd8b9fccba8f0d
woncz <woncz@sina.com>
2019-12-11 16:42:55 +0800

    algo4 week1 assignment percolation

---
+++ discover/algo4/Percolation.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright [2020]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package algo4;
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+/**
+ * key point : really understand the meaning of full-opened site
+ * (just like rain down, which can get water - my easy understanding)
+ */
+public class Percolation {
+
+    private final boolean[] canvas;
+
+    private final int size;
+
+    private int counter;
+
+    private final WeightedQuickUnionUF grid;
+
+    private final WeightedQuickUnionUF full;
+
+    private int top;
+
+    private int bottom;
+
+    private final int[] dx = {-1, 1, 0, 0};
+
+    private final int[] dy = {0, 0, -1, 1};
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+        if (n <= 0) throw new IllegalArgumentException();
+        size = n;
+        canvas = new boolean[n * n];
+
+        grid = new WeightedQuickUnionUF(size * size + 2);
+        full = new WeightedQuickUnionUF(size * size + 1); // prevent backwash
+
+        top = reduct(size, size) + 1;
+        bottom = reduct(size, size) + 2;
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        validate(row, col);
+
+        if (isOpen(row, col)) return;
+
+        int point = reduct(row, col);
+
+        counter++;
+        canvas[point] = true;
+
+        if (row == 1) {
+            grid.union(top, point);
+            full.union(top, point);
+        }
+        if (row == size) {
+            grid.union(bottom, point);
+        }
+
+        for (int i = 0; i < dx.length; i++) {
+            int nRow = row + dx[i];
+            int nCol = col + dy[i];
+            if (nRow > 0 && nRow <= size && nCol > 0 && nCol <= size && isOpen(nRow, nCol)) {
+                int neighbour = reduct(nRow, nCol);
+                grid.union(point, neighbour);
+                full.union(point, neighbour);
+            }
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        validate(row, col);
+        return canvas[reduct(row, col)];
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        validate(row, col);
+        return full.connected(top, reduct(row, col));
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        return counter;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return grid.connected(top, bottom);
+    }
+
+    /**------------ below is private method ------------------*/
+
+    // validate the point
+     private void validate(int row, int col) {
+         if (row <= 0 || row > size || col <= 0 || col > size) throw new IllegalArgumentException();
+     }
+
+     private int reduct(int row, int col) {
+         return (row - 1) * size + (col - 1);
+     }
+
+    // test client (optional)
+    public static void main(String[] args) {
+        Percolation p = new Percolation(4);
+        p.open(1, 1);
+        System.out.println(p.percolates());
+
+        p.open(2, 1);
+        p.open(3, 1);
+        System.out.println(p.percolates());
+
+        p.open(4, 1);
+        System.out.println(p.percolates());
+
+        System.out.println(p.isFull(4, 1));
+        System.out.println(p.isFull(4, 2));
+
+        p.open(4, 3);
+        p.open(4, 2);
+        System.out.println(p.isFull(4, 3));
+
+        p = new Percolation(4);
+        p.open(1, 2);
+        p.open(2, 2);
+        p.open(3, 2);
+        p.open(4, 2);
+        System.out.println(p.percolates());
+
+        p = new Percolation(4);
+        p.open(1, 1);
+        p.open(2, 1);
+        p.open(2, 2);
+        p.open(2, 3);
+        p.open(3, 3);
+        p.open(3, 4);
+        System.out.println(p.percolates());
+        p.open(4, 4);
+        System.out.println(p.percolates());
+    }
+}
---
+++ discover/algo4/PercolationStats.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright [2020]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package algo4;
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+
+    private final double mean;
+
+    private final double stddev;
+
+    private final double confidenceLo;
+
+    private final double confidenceHi;
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) throw new IllegalArgumentException();
+
+        double[] threshold = new double[trials];
+
+        for (int i = 0; i < trials; i++) {
+            Percolation percolation = new Percolation(n);
+            while (!percolation.percolates()) {
+                // [0, n * n)
+                int point = StdRandom.uniform(n * n);
+                int nRow = point / n + 1;
+                int nCol = point % n + 1;
+                percolation.open(nRow, nCol);
+            }
+            threshold[i] = (double) percolation.numberOfOpenSites() / ( n * n);
+        }
+
+        mean = StdStats.mean(threshold);
+        stddev = StdStats.stddev(threshold);
+        double nn = Math.sqrt(trials);
+        confidenceHi = mean + 1.96 * stddev / nn;
+        confidenceLo = mean - 1.96 * stddev / nn;
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return mean;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return stddev;
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return confidenceLo;
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return confidenceHi;
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        long start = System.currentTimeMillis();
+        PercolationStats stats = new PercolationStats(n, trials);
+
+        System.out.println("mean                        = " + stats.mean());
+        System.out.println("stddev                      = " + stats.stddev());
+        System.out.println("95% confidence interval     = [" + stats.confidenceLo() + ", " + stats.confidenceHi() + "]");
+
+        long end = System.currentTimeMillis();
+        System.out.println("total cost = " + (end - start) / 1000.0 + " seconds");
+    }
+}
